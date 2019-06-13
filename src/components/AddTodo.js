ae42ff98b9ff92088dfb7a0906d71745766aafdb
Temitope <temmieayodele@gmail.com>
2019-06-13 16:15:25 +0100

    added state

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -4,11 +4,14 @@ class AddTodo extends Component {
     state={
         title: ''
     }
+    onChange=(e)=> this.setState({
+        title: e.target.value
+    })
   render() {
     return (
       <div>
         <form style={{display:'flex'}}>
-          <input type="text" name="title" placeholder="Add Todo..." title={this.state.value} style={{flex:"10", padding: "5px"}}/>
+          <input type="text" name="title" placeholder="Add Todo..." title={this.state.value} onChange={this.onChange} style={{flex:"10", padding: "5px"}}/>
           <input type="submit" value="Submit" className="btn" style={{flex:"1"}}/>
         </form>
       </div>
