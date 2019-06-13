2c409a59549efee380401a06fdd8c054c4b3296a
Temitope <temmieayodele@gmail.com>
2019-06-13 14:49:50 +0100

    added more props

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,9 +3,10 @@ import TodoItem from './Todoitem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component{
+
   render(){
     return this.props.todos.map((todo)=>(
-        <TodoItem key={todo.id} todo={todo}/>
+        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo}/>
     ));
   }
 }
