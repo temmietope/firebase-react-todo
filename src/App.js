2caebc073a5a3bb5b99dc6d9e1abb3304a8c5178
Temitope <temmieayodele@gmail.com>
2019-06-13 14:45:39 +0100

    added delTodo and markComplete functions

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import React, {Component} from 'react';
-import Todos from './components/Todos'
+import React, { Component } from "react";
+import Todos from "./components/Todos";
 
-class App extends Component{
+class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        title: 'Take out the trash',
-        completed: false,
+        title: "Take out the trash",
+        completed: false
       },
       {
         id: 2,
-        title: 'Dinner with Wife',
-        completed: false,
+        title: "Dinner with Wife",
+        completed: false
       },
       {
         id: 3,
-        title: 'Meeting with Boss',
-        completed: false,
+        title: "Meeting with Boss",
+        completed: false
       }
     ]
+  };
+
+  //toggle completed
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo=>{
+      if( todo.id===id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  };
+  //delete Todo
+  delTodo = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo=>todo.id!==id)]})
+
   }
-  render(){
+  render() {
     return (
       <div className="App">
-      <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
 }
-
 
 export default App;
