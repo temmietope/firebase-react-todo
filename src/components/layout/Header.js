9666ae50a5ed6acc1252792293f6781c120f5c33
Temitope <temmieayodele@gmail.com>
2019-06-15 22:08:26 +0100

    add home link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
+      <Link to="/">Home</Link>
     </header>
   );
 }
