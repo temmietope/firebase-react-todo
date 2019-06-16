1f5c4da7b4547ddf1dc2bb47a4e7a513d04c78e7
Temitope <temmieayodele@gmail.com>
2019-06-16 19:21:34 +0100

    formatted code

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
-      <Link to="/">Home</Link>
+      <Link style={linkStyle} to="/"> Home </Link>{" "}|{" "} <Link style={linkStyle} to="/about"> About</Link>
     </header>
   );
 }
@@ -16,5 +16,8 @@ const headerStyle = {
   textAlign: "center",
   padding: "10px"
 };
-
+const linkStyle = {
+  color: "#fff",
+  textDecoration: "none"
+};
 export default Header;
