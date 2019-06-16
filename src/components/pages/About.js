dad5d4af16976c2460b03a8791d0bcada088729d
Temitope <temmieayodele@gmail.com>
2019-06-16 19:22:50 +0100

    formatted code

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,13 @@
-import React from 'react'
-function About(){
-    return(
-        <React.Fragment>
-            <h1>About</h1>
-            <p>This is the TodoList app v1.0.0. It is my first React Tutorial project
-            </p>
-        </React.Fragment>
-    )
+import React from "react";
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the TodoList app v1.0.0. It is my first React Tutorial project
+      </p>
+    </React.Fragment>
+  );
 }
 
-export default About
+export default About;
