707380fdc89c9d35c8990652e3c533c10da35b74
Temitope <temmieayodele@gmail.com>
2019-06-16 19:20:59 +0100

    formatted code

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
-    getStyle=()=>{
-        return{
-            padding: '10px',
-            borderBottom: '1px #ccc dotted',
-            textDecoration: this.props.todo.completed? "line-through":"none"
-        }
-    }
-    
-    render() {
-        const{ id, title } = this.props.todo
-        return (
-            <div style={this.getStyle()}>
-                <p>
-                <input type='checkbox' onChange={this.props.markComplete.bind(this, id)}/> {' '}
-                {title }
-                <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>x</button>
-                </p>
-            </div>
-        )
-    }
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none"
+    };
+  };
+  
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+          <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>
+            x
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
 
 //Proptypes
 TodoItem.propTypes = {
-    todo: PropTypes.object.isRequired
-  }
-const btnStyle={
-    backgroundColor: '#ff0000',
-    color: '#fff',
-    border: 'none',
-    padding: '5px 10px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right'
-}
-// const itemStyle = {
-//     backgroundColor: "#f4f4f4"
-// }
-export default TodoItem
+  todo: PropTypes.object.isRequired
+};
+const btnStyle = {
+  backgroundColor: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
+};
+
+export default TodoItem;
