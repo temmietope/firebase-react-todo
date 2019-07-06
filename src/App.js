1c9a8ae9819f62e7293ed02d20b28c9d2ace86ba
Temitope <temmieayodele@gmail.com>
2019-07-06 21:38:19 +0100

    now a Router file

--- src/App.js
+++ src/App.js
@@ -1,64 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
+import Home from "./components/Home";
 import About from "./components/pages/About";
-import { rand } from "./utils/random";
+import "./App.css";
 
 class App extends Component {
-  state = {
-    todos: []
-  };
-
-  //toggle completed
-  markComplete = id => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      })
-    });
-  };
-  //delete Todo
-  delTodo = id => {
-    const todos = this.state.todos.filter(todo => todo.id !== id);
-    this.setState({ todos });
-  };
-  //add Todo
-  addTodo = title => {
-    const newTodo = {
-      id: rand(),
-      title: title,
-      completed: false
-    };
-    this.setState({ todos: [...this.state.todos, newTodo] });
-  };
   render() {
     return (
       <Router>
-        <div className="App">
-          <div className="container">
-            <Header />
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <React.Fragment>
-                  <AddTodo addTodo={this.addTodo} />
-                  <Todos
-                    todos={this.state.todos}
-                    markComplete={this.markComplete}
-                    delTodo={this.delTodo}
-                  />
-                </React.Fragment>
-              )}
-            />
-            <Route path="/about" component={About} />
-          </div>
-        </div>
+        <>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </>
       </Router>
     );
   }
